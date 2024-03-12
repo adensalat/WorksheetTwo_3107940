9ea2b2560b7abe295f95cb4498587e12cc832715
adensalat <adenibrahim.salat@student.griffith.ie>
2024-03-12 18:16:30 +0000

    Implement celsiusToFahrenheit method

--- Conversions.java
+++ Conversions.java
@@ -2,4 +2,38 @@ package griffith;
 
 public class Conversions {
 
+    public double celsiusToFahrenheit() {
+        
+        return 0.0;
+    }
+
+    public double fahrenheitToCelsius() {
+        
+        return 0.0;
+    }
+
+    public double milesToKilometer() {
+       
+        return 0.0;
+    }
+
+    public double kilometerToMiles() {
+        
+        return 0.0;
+    }
+
+    public String upperCaseToLowerCase(String input) {
+        
+        return null;
+    }
+
+    public String lowerCaseToUpperCase(String input) {
+        
+        return null;
+    }
+    
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9; 
+    
+}
 }
