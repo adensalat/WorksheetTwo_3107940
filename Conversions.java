bb5931482008f55ab8f23cafa6ae15027ad5e5bc
adensalat <adenibrahim.salat@student.griffith.ie>
2024-03-13 13:37:29 +0000

    Created Conversions.java and ConversionsTest.java and also added method stubs to Conversions.java

--- Conversions.java
+++ Conversions.java
@@ -1,39 +1,37 @@
 package griffith;
 
+
 public class Conversions {
 
-    public double celsiusToFahrenheit() {
-        
+    public double celsiusToFahrenheit(double celsius) {
+        // Method stub
         return 0.0;
     }
 
-    public double fahrenheitToCelsius() {
-        
+    public double fahrenheitToCelsius(double fahrenheit) {
+        // Method stub
         return 0.0;
     }
 
-    public double milesToKilometer() {
-       
+    public double milesToKilometer(double miles) {
+        // Method stub
         return 0.0;
     }
 
-    public double kilometerToMiles() {
-        
+    public double kilometerToMiles(double kilometer) {
+        // Method stub
         return 0.0;
     }
 
     public String upperCaseToLowerCase(String input) {
-        
+        // Method stub
         return null;
     }
 
     public String lowerCaseToUpperCase(String input) {
-        
+        // Method stub
         return null;
     }
-    
-    public double fahrenheitToCelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5 / 9; 
-    
 }
-}
+
+
--- ConversionsTest.java
+++ ConversionsTest.java
@@ -1,5 +1,39 @@
 package griffith;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ConversionsTest {
 
+	@Test
+    public void testCelsiusToFahrenheit() {
+        
+    }
+
+    @Test
+    public void testFahrenheitToCelsius() {
+        
+    }
+
+    @Test
+    public void testMilesToKilometer() {
+        
+    }
+
+    @Test
+    public void testKilometerToMiles() {
+        
+    }
+
+    @Test
+    public void testUpperCaseToLowerCase() {
+        
+    }
+
+    @Test
+    public void testLowerCaseToUpperCase() {
+        
+    }
+   
 }
+
